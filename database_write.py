62bfefaec9a5faa0d494758fbfb92743bb4f4bec
remzore <remzore@gmail.com>
2018-01-17 18:47:55 +0200

    Write to db was added

---
+++ database_write.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+from settings import MONGODB_SERVER, MONGODB_PORT, MONGODB_DB, MONGODB_COLLECTION
+
+
+class SaveToMDB:
+    def __init__(self):
+        self.client = MongoClient(MONGODB_SERVER, MONGODB_PORT)
+        self.db = self.client[MONGODB_DB]
+        self.collection = self.db[MONGODB_COLLECTION]
+
+    def insert(self, dict):
+        return self.collection.insert_one(dict).inserted_id
+
+
+class InsertData:
+    def __init__(self, title, text, link, date):
+        self.title = title
+        self.text = text
+        self.link = link
+        self.date = date
+
+    def to_dict(self):
+        return {"title": self.title,
+                "text": self.text,
+                "link": self.link,
+                "date": self.date}
+
+
+save_to_mdb = SaveToMDB()
+
+
+def insert(db, dict):
+    return db.collection.insert_one(dict).inserted_id
---
+++ settings.py
@@ -0,0 +1,4 @@
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "news_ch"
+MONGODB_COLLECTION = "unian"
